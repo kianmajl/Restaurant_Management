d7f586432b74f7ae523376912e5ffa1368776ef7
Kian Majlessi <kianmajl@gmail.com>
2022-01-09 22:21:35 +0330

    Reading from file completed.

--- customer.py
+++ customer.py
@@ -1,1 +1,12 @@
-MAP_NUMBER = 0
+import os
+import heapq
+
+MAP_NUMBER = 6
+TABLE_NAMES = list()
+with open(os.path.join("Maps", "data" + str(MAP_NUMBER) + ".txt"), "r") as map_input:
+    for line in map_input:
+        for ch in line:
+            if ch.isalpha():
+                TABLE_NAMES.append(ch)
+
+print(TABLE_NAMES)
