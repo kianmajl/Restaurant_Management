0881af9684bb0f6642e081fe378a854c884a406a
Adrina <adrina.ei24@gmail.com>
2022-01-15 00:36:27 +0330

    Add delete and print part

--- avl.py
+++ avl.py
@@ -1,3 +1,6 @@
+COUNT = [10]
+
+
 class Person(object):
     def __init__(self, key, name=""):
         self.key = key
@@ -81,18 +84,100 @@ class AVLTree(object):
         else:
             return AVLTree.search(root.right, key)
 
+    @staticmethod
+    def get_min_value_node(root):
+        if root is None or root.left is None:
+            return root
+        return AVLTree.get_min_value_node(root.left)
+
+    def delete_node(self, root, key):
+        if not root:
+            return root
+        elif key < root.key:
+            root.left = self.delete_node(root.left, key)
+        elif key > root.key:
+            root.right = self.delete_node(root.right, key)
+        else:
+            if root.left is None:
+                temp = root.right
+                root = None
+                return temp
+            elif root.right is None:
+                temp = root.left
+                root = None
+                return temp
+            temp = self.get_min_value_node(root.right)
+            root.key = temp.key
+            root.right = self.delete_node(root.right, temp.key)
+        if root is None:
+            return root
+
+        root.height = 1 + max(self.get_height(root.left), self.get_height(root.right))
+
+        balance_factor = self.get_balance(root)
+
+        if balance_factor > 1:
+            if self.get_balance(root.left) >= 0:
+                return self.right_rotate(root)
+            else:
+                root.left = self.left_rotate(root.left)
+                return self.right_rotate(root)
+        if balance_factor < -1:
+            if self.get_balance(root.right) <= 0:
+                return self.left_rotate(root)
+            else:
+                root.right = self.right_rotate(root.right)
+                return self.left_rotate(root)
+        return root
+
+    @staticmethod
+    def print(root, space):
+
+        if not root:
+            return
+
+        space += COUNT[0]
+
+        AVLTree.print(root.right, space)
+
+        print()
+        for i in range(COUNT[0], space):
+            print(end=" ")
+        print(root.name)
+
+        AVLTree.print(root.left, space)
+
+    @staticmethod
+    def print2D(root):
+        AVLTree.print(root, 0)
+
 
 if __name__ == "__main__":
     customers, root_tree, customers_num = AVLTree(), None, dict()
 
     while True:
-        orders = input("Please Enter Your Order: ").split()
+        try:
+            orders = input("Please Enter Your Order: ").split()
 
-        if orders[0] == "Insert":
-            customers_num[orders[1]] = int(orders[2])
-            # noinspection PyTypeChecker
-            root_tree = customers.insert(root_tree, int(orders[2]), orders[1])
+            if orders[0] == "Insert":
+                customers_num[orders[1]] = int(orders[2])
+                # noinspection PyTypeChecker
+                root_tree = customers.insert(root_tree, int(orders[2]), orders[1])
 
-        elif orders[0] == "Search":
-            # noinspection PyTypeChecker
-            print(customers.search(root_tree, int(orders[1])))
+            elif orders[0] == "Search":
+                # noinspection PyTypeChecker
+                try:
+                    print(customers.search(root_tree, int(orders[1])))
+
+                except ValueError:
+                    print("Please enter a number after \"Search\"")
+
+            elif orders[0] == "Delete":
+                # noinspection PyTypeChecker
+                root_tree = customers.delete_node(root_tree, customers_num[orders[1]])
+
+            elif orders[0] == "Print":
+                customers.print2D(root_tree)
+
+        except IndexError:
+            print("Enter a valid order")
