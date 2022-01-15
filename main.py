5bc3b35b25e8c446e5f69e7468ddfcb32c9506fa
Adrina <adrina.ei24@gmail.com>
2022-01-15 16:29:38 +0330

    Add short menu

--- main.py
+++ main.py
@@ -3,4 +3,20 @@ import customer
 import kitchen
 
 if __name__ == "__main__":
-    kitchen.main()
+
+    while True:
+
+        print("\n1. Usual mood reservation")
+        print("\n2. Party mood reservation")
+
+        try:
+            selection = int(input("\n Please enter a number: "))
+
+            if selection == 1:
+                kitchen.main()
+
+            elif selection == 2:
+                kitchen.main()
+
+        except ValueError:
+            input("\nOops!  That was no valid number.  Try again...")
